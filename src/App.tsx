b22923534472ac3e37e2bc3454ba083b4cd875b7
wikomoko <mokoindonesia45@gmail.com>
2024-10-21 23:25:00 +0700

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { Button, Stack } from "@mui/material"
+
+
+function App() {
+ 
+
+  return (
+    <Stack spacing={3} direction='row'>
+     <Button variant="contained">Just Testing</Button>
+     <Button variant="contained">Just Testing</Button>
+    </Stack>
+  )
+}
+
+export default App
